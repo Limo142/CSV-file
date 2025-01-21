dcccf72ea1f344e7da1a1d9333a0e96210be55d1
Liam Molloy <liammolloy@Liams-MacBook-Pro.local>
2025-01-21 22:13:35 +1100

    Add CSV.py file

---
+++ CSV.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Ask your CSV 📈")
+    st.header("Ask your CSV 📈")
+
+    user_csv = st.file_uploader("Upload your CSV file", type="csv")
+
+
+
+    if __name__ == "__main__":
+        main()
